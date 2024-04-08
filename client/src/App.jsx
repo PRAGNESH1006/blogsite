f840dd410a91c462b13788edaf9c18763762177f
PRAGNESH1006 <pragneshpadhiyar4@gmail.com>
2024-04-08 18:26:50 +0530

    Create Pages and browse

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="/singup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
