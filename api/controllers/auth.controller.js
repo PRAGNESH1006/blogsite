90eca989c0bd6c676fb1f520c6aa4c42b66d6d0d
PRAGNESH1006 <pragneshpadhiyar4@gmail.com>
2024-04-11 00:22:01 +0530

    create sign in api and routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
+//sign up
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -12,7 +14,7 @@ export const signup = async (req, res, next) => {
     username === "" ||
     password === ""
   ) {
-    next(errorHandler(400, "All Feilds are required")); // from utils.js
+    return next(errorHandler(400, "All Feilds are required")); // from utils.js
   }
 
   // for creating new users
@@ -28,6 +30,43 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("You'r Signup has been successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// sign in with credentials
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All Feilds are required")); // from utils.js
+  }
+  // for finding user by email
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found")); // from utils.js
+    }
+
+    // for checking the password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Password is incorrect")); // from utils.js
+    }
+    // for checking the password and validating the password
+    const secretToken = "processenvSECRETTOKEN";
+    const token = jwt.sign({ id: validUser._id }, secretToken, {
+      expiresIn: 86400, // expires in 24 hours
+    });
+
+    const { password: pass, ...rest } = validUser._doc; //for hiding the password
+
+    // for showing the user data and storing the user data in cookies form on browser side
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
   } catch (error) {
     next(error);
   }
