5ef7711341033a7728c0ddbd536bd2c660d06ac1
PRAGNESH1006 <pragneshpadhiyar4@gmail.com>
2024-04-18 18:55:32 +0530

    Complete Image upload

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,44 +1,46 @@
-import { Button, TextInput } from 'flowbite-react'
+
+import { Alert, Button, TextInput } from 'flowbite-react'
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 import { app } from '../firebase'
 // import { FaPlusCircle } from "react-icons/fa";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashProfile() {
 
     const { currentUser } = useSelector((state) => state.user);
-    const [updateImage, setUpdatedImage] = useState(null)
-    const [imgUrl, setImgUrl] = useState(null)
-    const [imgUploadingState, setImgUploadingState] = useState(null)
-    const [imgUploadingError, setImgUploadingError] = useState(null)
-    const refUrl = useRef()// for refrencing purposes and hides to file input for profile photos
-
-    console.log(imgUploadingState, imgUploadingError)
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    const [imageFileUploading, setImageFileUploading] = useState(false);
+    const refUrl = useRef() // for refrencing purposes and hides to file input for profile photos
 
 
     function handleImageUpdate(e) {
         const file = e.target.files[0]
         if (file) {
-            setUpdatedImage(file)
-            setImgUrl(URL.createObjectURL(file))
-            console.log(updateImage)
+            setImageFile(file)
+            setImageFileUrl(URL.createObjectURL(file))
+
         }
     }
 
 
 
     useEffect(() => {
-        if (updateImage) {
+        if (imageFile) {
             uploading()
         }
-    }, [updateImage])
+    }, [imageFile])
 
 
     // uploading img using firebase
     const uploading = async () => {
         const storage = getStorage(app)
-        const fileName = new Date().getTime() + updateImage.name
+        const fileName = new Date().getTime() + imageFile.name
         const storageRef = ref(storage, fileName)
         const uploadTask = uploadBytesResumable(storageRef, fileName)
         uploadTask.on(
@@ -46,20 +48,33 @@ export default function DashProfile() {
             (snapshot) => {
                 const progress =
                     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                setImgUploadingState(progress.toFixed(0))
+                setImageFileUploadProgress(progress.toFixed(0))
             },
             (error) => {
-                setImgUploadingError(error)
+                setImageFileUploadError(
+                    'Could not upload image (File must be less than 2MB)'
+                );
+                setImageFileUploadProgress(null)
+                setImageFile(null);
+                setImageFileUrl(null);
+                console.log(error)
             },
             () => {
                 getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
-                    setImgUrl(downloadUrl)
+                    setImageFileUrl(downloadUrl)
                 })
 
             }
         )
 
     }
+
+
+    console.log(imageFile)
+    console.log(imageFileUrl)
+    console.log(imageFileUploadProgress)
+    console.log(imageFileUploadError)
+    console.log(imageFileUploading)
 
 
     return (
@@ -70,10 +85,31 @@ export default function DashProfile() {
 
                     <input type='file' accept='image/*' className='flex justify-center items-center ' onChange={handleImageUpdate} ref={refUrl} style={{ display: 'none' }} />
 
-                    <div className="h-32 w-32 self-center flex  " onClick={() => { refUrl.current.click() }}>
-                        <img src={imgUrl || currentUser.profilephoto} alt="user" className=' cursor-pointer w-full h-full  rounded-full border-8 border-[gray] ' />
+
+                    <div className="h-32 w-32 self-center flex relative " onClick={() => { refUrl.current.click() }}>
+                        {
+                            imageFileUploading && <CircularProgressbar value={imageFileUploading || 0} text={`${imageFileUploading}%`}
+                                strokeWidth={5}
+                                styles={{
+                                    root: {
+                                        width: '100%',
+                                        height: '100%',
+                                        position: "absolute",
+                                        top: 0,
+                                        left: 0
+                                    },
+                                    path: {
+                                        stroke: `rgba(62,152,199,${imageFileUploadProgress / 100}`
+                                    }
+                                }}
+
+                            />}
+                        <img src={imageFileUrl || currentUser.profilephoto} alt="user" className={`cursor-pointer w-full h-full  rounded-full border-8 border-[gray] ${imageFileUploadProgress && imageFileUploadProgress < 100 && 'opacity-60'} `} />
                     </div>
                     {/* <FaPlusCircle className='gap-0' /> */}
+                    {imageFileUploadError && (
+                        <Alert color="failure"> {imageFileUploadError} </Alert>)}
+
                     <div className='my-2'>
                         <TextInput type='text' id='username' defaultValue={currentUser.username} placeholder='username' className='my-2 ' readOnly />
                         <TextInput type='email' id='email' defaultValue={currentUser.email} placeholder='email' className='my-2' />
